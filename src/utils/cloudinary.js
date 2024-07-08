1e281f221c64b322a72647b84612f6eb437eb37b
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-07-08 14:31:51 +0600

    console.log() removed from cloudinary.js file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     })
-    console.log("File is uploaded on cloudinary, ", response.url);
+    
     fs.unlinkSync(localFilePath);
     return response;
     
